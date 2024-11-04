15f711c838c054c6f7459c2b026a4c28f8050951
sohan-99 <sohan75632@gmail.com>
2024-11-05 00:09:43 +0600

    update user's profile info & picture

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { loginUser, registerUser, userProfile } from "../controllers/userControllers.js"; 
+import { loginUser, registerUser, updateProfile, updateProfilePicture, userProfile } from "../controllers/userControllers.js"; 
 import { authGuard } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/profile",authGuard, userProfile);
+router.put("/updateProfile", authGuard, updateProfile);
+router.put("/updateProfilePicture", authGuard, updateProfilePicture);
 export default router;
