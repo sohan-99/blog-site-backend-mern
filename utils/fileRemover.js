596fe8674deb705041cf2b8770ed9db0b8c9230b
sohan-99 <sohan75632@gmail.com>
2024-11-05 00:19:21 +0600

    fixing bugs

--- utils/fileRemover.js
+++ utils/fileRemover.js
@@ -1,7 +1,12 @@
 import fs from "fs";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const fileRemover = (filename) => {
-  fs.unlink(path.join(__dirname, "../uploads"), function (err) {
+  fs.unlink(path.join(__dirname, "../uploads", filename), function (err) {
     if (err && err.code == "ENOENT") {
       // file doesn't exist
       console.log(`File ${filename} doesn't exist, won't remove it.`);
@@ -13,4 +18,5 @@ const fileRemover = (filename) => {
     }
   });
 };
+
 export { fileRemover };
